6a5875bd1c1af73c2520f2210b8b752467895765
sayo-melocojon <145472048+sayo-melocojon@users.noreply.github.com>
2023-11-30 08:48:54 -0600

    Add files via upload

---
+++ persona/src/main/java/com/mycompany/persona/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persona;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
